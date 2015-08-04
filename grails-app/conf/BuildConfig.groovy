f5805ff64f17ee793bda8d80fe1c9e9010842c39
vamshins <vamshi.krishna588@gmail.com>
2015-08-04 13:19:22 -0600

    Changed to Hibernate ver: 3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,8 +64,8 @@ grails.project.dependency.resolution = {
         compile ":mysql-connectorj:5.1.22.1"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
-//        runtime ":hibernate:3.6.10.18"
+//        runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
+        runtime ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
